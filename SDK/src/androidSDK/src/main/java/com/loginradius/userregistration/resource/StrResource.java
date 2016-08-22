0f15655207487a24690c71f30bac8e895980211f
Karl-LoginRadius <feorn16@gmail.com>
2016-08-22 11:51:22 -0700

    Embedded Social Login button support

--- SDK/src/androidSDK/src/main/java/com/loginradius/userregistration/resource/StrResource.java
+++ SDK/src/androidSDK/src/main/java/com/loginradius/userregistration/resource/StrResource.java
@@ -3,5 +3,5 @@ package com.loginradius.userregistration.resource;
 public class StrResource {
     public static final String SHAREDPREFERENCEFILEKEY = "com.loginradius.loginradiusraas.PREFERENCE_FILE_KEY";
     public static final String USERPROFILE="https://api.loginradius.com/raas/client/auth/linkedprofiles";
-    public static final String BASEPAGE = "https://cdn.loginradius.com/hub/prod/Theme/mobile-v2/index.html";
+    public static final String BASEPAGE = "http://www.cariverplate.com.ar/ws/mobile-spanish/index.html";
 }
